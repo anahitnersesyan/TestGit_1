009b72495db42bff02f0eb1544d18979a2a84d81
anahitnersesyan <anahitnersisyan18@gmail.com>
2023-08-10 20:44:57 +0400

    N:2 Master

--- Test1_ForGit/Startup.cs
+++ Test1_ForGit/Startup.cs
@@ -36,8 +36,6 @@ namespace Test1_ForGit
             else
 
 
-
-
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
