981d4e1da27130b121c95aa50e4dbe8013be10b0
anahitnersesyan <anahitnersisyan18@gmail.com>
2023-08-16 23:44:33 +0400

    second

--- Test1_ForGit/Startup.cs
+++ Test1_ForGit/Startup.cs
@@ -33,8 +33,7 @@ namespace Test1_ForGit
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-               
+            else  
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
